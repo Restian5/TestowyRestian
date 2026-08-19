a3e945b7416c5b64d5dc71a9806c88a7c98eb32c
Restian5 <126951125+Restian5@users.noreply.github.com>
2026-08-19 14:10:34 +0200

    Update wklejki-fitmeet-emplo.user.js

--- wklejki-fitmeet-emplo.user.js
+++ wklejki-fitmeet-emplo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EmploMaster
-// @namespace    wklejki-fitmeet
-// @version      4.6
+// @namespace    EmploMaster
+// @version      1.0
 // @author       MJU
 // @description  Automatyczne formatki (wklejki) na stronie wniosku emplo — rozpoznaje wnioski o dostęp do Perfect Gym
 // @match        https://bsof.emplo.com/*
